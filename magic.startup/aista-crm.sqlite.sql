df97f8fd476f35f604a4d007d3d3d845418b9fef
Aista, Ltd <th@aista.com>
2022-10-15 11:10:37 +0300

    b

--- magic.startup/aista-crm.sqlite.sql
+++ magic.startup/aista-crm.sqlite.sql
@@ -29,6 +29,7 @@ create table accounts(
   created timestamp not null default current_timestamp,
   name text not null,
   website text,
+  description text,
   account_manager text /* Username of employee responsible for following up on account */
 );
 
